23b67565ea024edecf0fcb705ce2bad3fbd9c84c
BeraSoutik7 <soutik.bera02@gmail.com>
2024-02-25 20:47:36 +0530

    My third change

--- SimpleJavaCode.java
+++ SimpleJavaCode.java
@@ -2,5 +2,6 @@ public class SimpleJavaCode {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         System.out.println("New change");
+        System.out.println("Another change");
     }
 }
